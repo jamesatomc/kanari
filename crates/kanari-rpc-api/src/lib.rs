cc55f4cd989f91b207f00746860bf3d0e3d939d4
jamesatomc <jiraphonwansai2002@gmail.com>
2025-07-22 22:26:15 +0700

    feat: add rooch-open-rpc crate with initial implementation and dependencies; update CI workflow to include rustfmt

--- crates/kanari-rpc-api/src/lib.rs
+++ crates/kanari-rpc-api/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Copyright (c) KanariNetwork
+// SPDX-License-Identifier: Apache-2.0
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
